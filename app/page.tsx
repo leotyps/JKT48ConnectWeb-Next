f90915223485007ab6c3c2760641137aaae8f8cb
leotyps <113692736+leotyps@users.noreply.github.com>
2025-05-21 18:07:53 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,14 +36,14 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <span className={title()}>Power up yout&nbsp;</span>
+        <span className={title({ color: "violet" })}>JKT48&nbsp;</span>
         <br />
         <span className={title()}>
-          websites regardless of your design experience.
+          project.
         </span>
         <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
+          Use JKT48Connect’s API and tools to build something fun.
         </div>
       </div>
 
@@ -57,7 +57,7 @@ export default function Home() {
           })}
           href={siteConfig.links.docs}
         >
-          Documentation
+          Read Docs
         </Link>
         <Link
           isExternal
