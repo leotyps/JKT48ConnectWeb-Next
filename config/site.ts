f68f6b5e84ce426c7496c4b3b9b4e512ca9331ea
leotyps <113692736+leotyps@users.noreply.github.com>
2025-05-21 21:43:11 +0700

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
+  name: "JKT48Connect - Valzyy",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
@@ -60,10 +60,10 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://github.com/heroui-inc/heroui",
-    twitter: "https://twitter.com/hero_ui",
-    docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
+    github: "https://github.com/j-forces",
+    twitter: "https://twitter.com/valzyys",
+    docs: "https://docs.jkt48connect.my.id",
+    discord: "https://discord.gg/pFsSeYa9CN",
     sponsor: "https://patreon.com/jrgarciadev",
   },
 };
