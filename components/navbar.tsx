eb973234ac290a86a775fe29f352e24cad8f4dd3
leotyps <113692736+leotyps@users.noreply.github.com>
2025-05-22 17:57:26 +0700

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -118,19 +118,21 @@ export const Navbar = () => {
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
+              <NextLink
+                className={clsx(
+                  linkStyles({ 
+                    color: index === 2
+                      ? "primary"
+                      : index === siteConfig.navMenuItems.length - 1
+                        ? "danger"
+                        : "foreground"
+                  }),
+                  "w-full"
+                )}
+                href={item.href}
               >
                 {item.label}
-              </Link>
+              </NextLink>
             </NavbarMenuItem>
           ))}
         </div>
