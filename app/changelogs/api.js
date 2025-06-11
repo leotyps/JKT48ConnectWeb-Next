e552548401cc0c1fa5fb211b3acb31976a0c1acc
leotyps <113692736+leotyps@users.noreply.github.com>
2025-06-11 10:15:30 +0700

    Update api.js

--- app/changelogs/api.js
+++ app/changelogs/api.js
@@ -3,14 +3,8 @@ import axios from 'axios';
 
 export const fetchChangelogs = async () => {
   try {
-    const response = await axios.get('https://v2.jkt48connect.my.id/api/database/changelogs', {
-      headers: {
-        'username': process.env.REACT_APP_USERNAME,
-        'password': process.env.REACT_APP_PASSWORD,
-        'apikey': process.env.REACT_APP_APIKEY
-      }
-    });
-    return response.data;
+    const response = await axios.get('https://v2.jkt48connect.my.id/api/database/changelogs?username=vzy&password=vzy&apikey=JKTCONNECT');
+    return response.data.data;
   } catch (error) {
     console.error('Error fetching changelogs:', error);
     throw error;
