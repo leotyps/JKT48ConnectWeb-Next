8b5ae544e528cf8ee820cc9de69605e7eaa05929
leotyps <113692736+leotyps@users.noreply.github.com>
2025-06-11 07:16:25 +0700

    Update api.js

--- app/changelogs/api.js
+++ app/changelogs/api.js
@@ -2,8 +2,21 @@
 import axios from 'axios';
 
 export const fetchChangelogs = async () => {
+  const username = process.env.REACT_APP_USERNAME;
+  const password = process.env.REACT_APP_PASSWORD;
+  const apiKey = process.env.REACT_APP_APIKEY;
+  const apiUrl = process.env.REACT_APP_API_URL;
+
   try {
-    const response = await axios.get('https://v2.jkt48connect.my.id/api/database/changelogs?username=vzy&password=vzy&apikey=JKTCONNECT');
+    // Pastikan header authorization diatur dengan benar sesuai dengan kebutuhan API Anda
+    const response = await axios.get(`${apiUrl}/changelogs`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'username': username,
+        'password': password,
+        'apikey': apiKey
+      }
+    });
     return response.data.data;
   } catch (error) {
     console.error('Error fetching changelogs:', error);
